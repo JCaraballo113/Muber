c7cb44b467bf678f233c65845f7e434b736a2b64
John <jcaraballo019@gmail.com>
2017-12-17 16:14:31 -0400

    Implementing GeoNear functionality

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
+  app.get('/api/drivers', DriversController.index);
+
   app.post('/api/drivers', DriversController.create);
 
   app.put('/api/drivers/:id', DriversController.edit);
