841bbce83e463d5ceab39c374703b3bb13bab454
John <jcaraballo019@gmail.com>
2017-12-17 17:03:39 -0400

    fixed up geoJSON queries

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -76,7 +76,8 @@ describe('Drivers Controller', () => {
       request(app)
       .get('/api/drivers?lng=-80&lat=25')
       .end((err, response) => {
-        console.log(response);
+        assert(response.body.length === 1);
+        assert(response.body[0].obj.email === 'miami@test.com');
         done();
       });
     });
